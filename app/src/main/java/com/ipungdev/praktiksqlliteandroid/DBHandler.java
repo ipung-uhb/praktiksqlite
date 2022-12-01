7d05d3890f947d5350306b958618babd1f60f421
unknown <purwono@uhb.ac.id>
2022-12-01 08:38:29 +0700

    DB Handler Array List

--- app/src/main/java/com/ipungdev/praktiksqlliteandroid/DBHandler.java
+++ app/src/main/java/com/ipungdev/praktiksqlliteandroid/DBHandler.java
@@ -2,8 +2,11 @@ package com.ipungdev.praktiksqlliteandroid;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 public class DBHandler extends SQLiteOpenHelper {
 
@@ -82,6 +85,35 @@ public class DBHandler extends SQLiteOpenHelper {
         // at last we are closing our
         // database after adding database.
         db.close();
+    }
+
+    // we have created a new method for reading all the courses.
+    public ArrayList<CourseModal> readCourses() {
+        // on below line we are creating a
+        // database for reading our database.
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // on below line we are creating a cursor with query to read data from database.
+        Cursor cursorCourses = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+
+        // on below line we are creating a new array list.
+        ArrayList<CourseModal> courseModalArrayList = new ArrayList<>();
+
+        // moving our cursor to first position.
+        if (cursorCourses.moveToFirst()) {
+            do {
+                // on below line we are adding the data from cursor to our array list.
+                courseModalArrayList.add(new CourseModal(cursorCourses.getString(1),
+                        cursorCourses.getString(4),
+                        cursorCourses.getString(2),
+                        cursorCourses.getString(3)));
+            } while (cursorCourses.moveToNext());
+            // moving our cursor to next.
+        }
+        // at last closing our cursor
+        // and returning our array list.
+        cursorCourses.close();
+        return courseModalArrayList;
     }
 
     @Override
